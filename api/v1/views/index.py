b784a161e4c64f5b6276071f84abc35db8f4f8bd
Bayero-abdul <bayeroa65@gmail.com>
2023-07-28 18:31:20 +0100

    Add strict_slash=true and a docstring to function stat()

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,8 +16,11 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
+    """
+    retrieves the number of each objects by type
+    """
     amenities = storage.count("Amenity")
     cities = storage.count("City")
     places = storage.count("Place")
