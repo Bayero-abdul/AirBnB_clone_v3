f07e4b3b237015714bc678822bb9fed2344643bd
Bayero-abdul <bayeroa65@gmail.com>
2023-07-31 18:52:50 +0100

    Debug api/v1/views/states.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
 This module creates an endpoint that retrieves
-the number of each objects by type
+the number of each objects by type and returns the status of your API
 """
-from models import storage
+
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -14,10 +15,10 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """
-    Return the status of your API
+    Returns the status of your API
     """
     status = {"status": "OK"}
     return jsonify(status)
@@ -26,12 +27,13 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """
-    return dict count of data
+    Retrieves the number of each objects by type
     """
-    stats = {"amenities": storage.count(Amenity),
-             "cities": storage.count(City),
-             "places": storage.count(Place),
-             "reviews": storage.count(Review),
-             "states": storage.count(State),
-             "users": storage.count(User)}
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)}
     return jsonify(stats)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Module handling requests for State objects """
+"""
+Module handling requests for State objects
+"""
 
 from flask import jsonify, request, abort
 from api.v1.views import app_views
@@ -9,51 +11,78 @@ from models.state import State
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
+    """
+    Retrieves the list of all State objects
+    """
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_state(state_id):
+    """
+    Retrieves a State object
+    """
     state = storage.get(State, state_id)
     if state is None:
-        return jsonify({"error": "State not found"})
+        abort(404, "State not found")
+
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
+    """
+    Deletes a State object
+    """
     state = storage.get(State, state_id)
     if state is None:
-        return jsonify({"error": "State not found"})
+        abort(404, "State not found")
+
     storage.delete(state)
     storage.save()
-    return jsonify({})
+
+    return jsonify({}), 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    if not request.get_json():
-        return jsonify({"error": "Not a JSON"}), 400
-    if 'name' not in request.get_json():
-        return jsonify({"error": "Missing name"}), 400
+    """
+    Creates a State
+    """
     data = request.get_json()
-    new_state = State(**data)
+    if not data:
+        abort(400, "Not a JSON")
+
+    if 'name' not in data:
+        abort(400, "Missing name")
+
+    new_state = State(name=data['name'])
     new_state.save()
+
     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
+    """
+    Updates a State object
+    """
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+
     state = storage.get(State, state_id)
     if state is None:
-        return jsonify({"error": "State not found"})
-    if not request.get_json():
-        return jsonify({"error": "Not a JSON"}), 400
-    data = request.get_json()
+        abort(404, "State not found")
+
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
+
     state.save()
-    return jsonify(state.to_dict())
+
+    return jsonify(state.to_dict()), 200
